f8745c9f2b724d90ba147cb70716357bedace9d5
Roman <romereez@gmail.com>
2026-03-30 14:56:49 +0200

    init

--- playwright.config.js
+++ playwright.config.js
@@ -2,28 +2,24 @@ const { defineConfig, devices } = require("@playwright/test");
 require("dotenv").config();
 
 module.exports = defineConfig({
-  testDir: "./tests",
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  timeout: 30000,
   reporter: [["html", { open: "never" }]],
-
   use: {
-    baseURL: process.env.BASE_URL,
     trace: "on-first-retry",
-    screenshot: "only-on-failure",
-    video: "retain-on-failure",
-    launchOptions: {
-      args: ["--force-dark-mode"],
-    },
   },
-
   projects: [
     {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      name: "UI Test",
+      testDir: "./tests",
+      use: {
+        baseURL: process.env.BASE_URL,
+        ...devices["Desktop Chrome"],
+        launchOptions: {
+          args: ["--force-dark-mode"],
+        },
+      },
     },
   ],
+  timeout: 30 * 1000,
+  screenshot: "only-on-failure",
+  workers: 1,
 });
